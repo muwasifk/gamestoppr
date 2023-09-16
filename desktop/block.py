ddd62917428f1e9ba35d789ea0abffcfead4c2ea
Mingqing Dai <mingqing.dai.s@gmail.com>
2023-09-16 13:56:17 -0400

    Improved desktop app: implemented threading and prototype login system

--- desktop/block.py
+++ desktop/block.py
@@ -6,37 +6,42 @@ import cohere
 
 def game_blocker(stop_event):
     while not stop_event.is_set():
-        # get list of all running processes
-        all_processes = [psutil.Process(pid) for pid in psutil.pids()]
 
-        co = cohere.Client('CF7vW6fYsPurYDy2J1fvRov2O0iupNb0zjraIPUC')
+        client_env_config = Config(RepositoryEnv('.envClient'))
+        server_env_config = Config(RepositoryEnv('.envServer'))
 
-        with open("apps.txt", "r+") as f_app_list:
-            app_list = f_app_list.read().splitlines()
-            for i in range(len(app_list)):
-                app_list[i] = app_list[i].split(",")
+        if(client_env_config('TOKEN') != "None"):
+            # get list of all running processes
+            all_processes = [psutil.Process(pid) for pid in psutil.pids()]
 
-            apps = dict(app_list)
+            co = cohere.Client('CF7vW6fYsPurYDy2J1fvRov2O0iupNb0zjraIPUC')
 
-            # print various information about each process
-            for process in all_processes:
-                try:
-                    if "\\Windows\\" not in process.exe() and process.name() not in apps:
-                        response = co.generate(
-                            prompt = f"If {process.name()} is a game, return 1. Else, return 0.",
-                        )
-                        apps[process.name()] = response[0].text[-1]
-                        f_app_list.write(f"\n{process.name()},{response[0].text[-1]}")
+            with open("apps.txt", "r+") as f_app_list:
+                app_list = f_app_list.read().splitlines()
+                for i in range(len(app_list)):
+                    app_list[i] = app_list[i].split(",")
 
-                        #print(f"PID: {p.pid}, Name: {p.name()}, Exe: {p.exe()}, Memory Info: {p.memory_info()}, CPU percent: {p.cpu_percent()}")
-                except (psutil.NoSuchProcess, psutil.AccessDenied):
-                    pass
+                apps = dict(app_list)
 
-            for app in apps:
-                if apps[app] == 1:
+                # print various information about each process
+                for process in all_processes:
                     try:
-                        print(f"{app} is a game! Please close it to focus up!")
-                        # os.system(f'taskkill /F /IM {app}')
-                    except Exception as e:
-                        print(e)
-        time.sleep(120)
+                        if "\\Windows\\" not in process.exe() and process.name() not in apps:
+                            response = co.generate(
+                                prompt = f"If {process.exe()} is a file belonging to a game or if {process.name()} is a game file, return 1. Else, return 0.",
+                            )
+                            apps[process.name()] = response[0].text[-1]
+                            f_app_list.write(f"\n{process.name()},{response[0].text[-1]}")
+
+                            # print(f"PID: {p.pid}, Name: {p.name()}, Exe: {p.exe()}, Memory Info: {p.memory_info()}, CPU percent: {p.cpu_percent()}")
+                    except (psutil.NoSuchProcess, psutil.AccessDenied):
+                        pass
+
+                for app in apps:
+                    if apps[app] == 1:
+                        try:
+                            print(f"{app} is a game! Please close it to focus up!")
+                            # os.system(f'taskkill /F /IM {app}')
+                        except Exception as e:
+                            print(e)
+            time.sleep(120)
---
+++ desktop/check_server.py
@@ -0,0 +1,16 @@
+import time
+import requests
+
+from decouple import Config, RepositoryEnv
+
+def check_server(stop_event):
+
+    while not stop_event.is_set():
+
+        client_env_config = Config(RepositoryEnv('.envClient'))
+        server_env_config = Config(RepositoryEnv('.envServer'))
+        if(client_env_config('TOKEN') != "None"):
+            response = requests.post(server_env_config('CHECK'), headers={'Authorization': f"Token {client_env_config('TOKEN')}"}, data={"device_id": "test1"})
+            print(response.content)
+
+        time.sleep(300)
